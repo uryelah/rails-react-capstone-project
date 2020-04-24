857021d7df829997505ca4dea16b9a52953a6d28
sarah <sarachamorro@gmail.com>
2020-04-24 18:16:00 -0300

    Add api changes

--- api/db/migrate/20200423170104_create_users.rb
+++ api/db/migrate/20200423170104_create_users.rb
@@ -4,7 +4,6 @@ class CreateUsers < ActiveRecord::Migration[6.0]
       t.string :name
       t.string :email
       t.string :password_digest
-      t.references :meeting, optional: true, foreign_key: true
 
       t.timestamps
     end
--- api/db/migrate/20200424175028_remove_meeting_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveMeetingFromUsers < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :users, :meeting, optional: true, foreign_key: true
-  end
-end
