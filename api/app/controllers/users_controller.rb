28f0b6488ec2a58e3569eb8dd8eef8fd2d0669ef
sarah <sarachamorro@gmail.com>
2020-04-24 17:58:15 -0300

    Add api changes

--- api/app/controllers/users_controller.rb
+++ api/app/controllers/users_controller.rb
@@ -1,9 +1,8 @@
 class UsersController < ApplicationController
     skip_before_action :authorize_request, only: [:create, :index]
     def create
-        user = User.new(user_params)
-        user.picture = "https://fakeimg.pl/100x100/?text=#{user_params.name[0..1].upcase}&font=lobster&font_size=36"
-        user.save!
+        user = User.create!(user_params)
+
         auth_token = AuthenticateUser.new(user.email, user.password).call
         response = { message: Message.account_created, auth_token: auth_token }
         json_response(response, :created)
