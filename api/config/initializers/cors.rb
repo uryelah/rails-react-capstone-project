c624bb561dead401ce7baf3480284222cf5cea30
sarah <sarachamorro@gmail.com>
2020-04-30 18:07:43 -0300

    Add credentials true to cors api config

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -11,6 +11,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     resource(
       '*',
+      credentials: true,
       headers: :any,
       methods: [:get, :patch, :put, :delete, :post, :options, :head]
     )
