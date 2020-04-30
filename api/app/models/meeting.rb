1fc2165812c10a090bd43c2ccacc12399f00843b
sarah <sarachamorro@gmail.com>
2020-04-30 13:24:50 -0300

    Add app and api changes

--- api/app/models/meeting.rb
+++ api/app/models/meeting.rb
@@ -1,7 +1,7 @@
 class Meeting < ApplicationRecord
   belongs_to :meet
 
-  has_many :user_meetings
+  has_many :user_meetings, dependent: :destroy
   has_many :users, through: :user_meetings
   validates_presence_of :title
 end
--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
-  has_many :user_meets
-  has_many :user_meetings
+  has_many :user_meets, dependent: :destroy
+  has_many :user_meetings, dependent: :destroy
   has_many :meetings, through: :user_meetings
   has_many :meets, through: :user_meets
   has_many :created_meets, foreign_key: :created_by, class_name: 'Meet'
